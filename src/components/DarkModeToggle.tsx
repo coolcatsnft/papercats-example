af34eb4f2356a9c42c79906b30d8119b8b92683a
Alex <alex@coolcatsnft.com>
2022-05-20 18:36:32 +0100

    feat: make sure checkbox keeps in sync witth localStorage changes

--- src/components/DarkModeToggle.tsx
+++ src/components/DarkModeToggle.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import useLocalStorage from '../hooks/useLocalStorage';
 import useWatchLocalStorage from '../hooks/useWatchLocalStorage';
 
@@ -5,6 +6,13 @@ export function DarkModeToggle() {
   const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
   const [theme, setTheme] = useLocalStorage<string>('theme', defaultDark ? 'dark' : 'light');
   const darkMode = useWatchLocalStorage('theme', defaultDark ? 'dark' : 'light');
+
+  // Handle other browser change
+  useEffect(() => {
+    if (darkMode.value !== theme) {
+      setTheme(darkMode.value);
+    }
+  }, [darkMode, theme, setTheme]);
 
   return (
     <label data-theme={darkMode.value}>
