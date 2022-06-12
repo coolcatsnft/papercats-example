b720e714ed264e06ca455fb0ea48f012977a660c
= <=>
2022-06-12 22:58:00 +0100

    feat: finish chapter 4 example

--- chapter-4/src/App.js
+++ chapter-4/src/App.js
@@ -1,12 +1,42 @@
-import './App.css';
+import { useEffect, useState } from 'react';
 import Web3Widget from './Web3Widget';
 
 function App() {
+  const [address, setAddress] = useState('');
+  const [balance, setBalance] = useState('');
+  const [library, setLibrary] = useState(null);
+
+  useEffect(() => {
+    const handleWidgetEvent = (e) => {
+      if (!e || !e.detail || !e.detail.address || e.detail.status === 'disconnected') {
+        setAddress('');
+        setBalance('');
+        setLibrary(null);
+        return;
+      }
+
+      if (e.detail.address !== address) {
+        setAddress(e.detail.address);
+        setBalance(e.detail.balance);
+        setLibrary(e.detail.web3);
+      }
+    };
+
+    document.addEventListener('web3-widget-event', handleWidgetEvent);
+
+    return () => {
+      document.removeEventListener('web3-widget-event', handleWidgetEvent);
+    }
+  }, []);
+
   return (
     <div className="App">
-      My Paper Cats App!
+      <h1>My Paper Cats App!</h1>
 
       <Web3Widget />
+
+      {address && <div>Wallet: {address}</div>}
+      {balance && <div>Balance: {balance}</div>}
     </div>
   );
 }
