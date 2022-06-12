e52519ac92222b80f72b85f69551ff23f5ff4095
= <=>
2022-06-12 15:12:38 +0100

    feat: start of chapter 4

---
+++ chapter-4/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Web3Widget from './Web3Widget';
+
+function App() {
+  return (
+    <div className="App">
+      My Paper Cats App!
+
+      <Web3Widget />
+    </div>
+  );
+}
+
+export default App;
