060a18612ac80687e7a1f51da4976c0da87b93a3
Alex <alex@coolcatsnft.com>
2022-06-14 09:01:39 +0100

    chore: set html overflow-y property to avoid some jumpiness

--- final/src/App.tsx
+++ final/src/App.tsx
@@ -76,6 +76,10 @@ const GlobalStyle = createGlobalStyle`
     --button-text-primary: #EEE;
   }
 
+  html {
+    overflow-y: scroll;
+  }
+
   body {
     margin: 0;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
