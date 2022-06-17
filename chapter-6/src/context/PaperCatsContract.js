6e5cb26e413e7c5d83b13702e83f6b5aeaa27839
= <=>
2022-06-17 21:59:17 +0100

    feat: move abi fetch into a hook

--- chapter-6/src/context/PaperCatsContract.js
+++ chapter-6/src/context/PaperCatsContract.js
@@ -1,49 +1,12 @@
-import { createContext, useEffect, useState } from "react";
-import useWeb3 from "../hooks/useWeb3";
+import { createContext } from "react";
+import useFetchContract from "./useFetchContract";
 
 const PAPER_CATS_CONTRACT = '0xB574BC3b58fED191846D678fB1B0127d35832e9A';
 
 export const PaperCatsContractContext = createContext();
 
 export const PaperCatsContractProvider = ({ children }) => {
-  const { address, library } = useWeb3();
-  const [contract, setContract] = useState();
-  const [loadingContract, setLoadingContract] = useState(false);
-  const [contractError, setContractError] = useState();
-
-  useEffect(() => {
-    const fetchAbi = () => {
-      return fetch(`//api-rinkeby.etherscan.io/api?module=contract&action=getabi&address=${PAPER_CATS_CONTRACT}&format=raw`).then((res) => {
-        if (res.status !== 200) {
-          throw new Error('Error connecting to etherscan');
-        }
-        return res.json().then((json) => {
-          if (json.message === "NOTOK") {
-            throw new Error(json.result || 'Error parsing abi json');
-          }
-          
-          return json;
-        });
-      });
-    }
-    
-    if (address && library && !contract && !loadingContract && !contractError) {
-      const prom = fetchAbi();
-      setLoadingContract(true);
-      prom.then((json) => {
-        setLoadingContract(false);
-        setContract(new library.eth.Contract(json, PAPER_CATS_CONTRACT));
-      }).catch((err) => {
-        setContractError(err.toString());
-        setLoadingContract(false);
-      });
-    }
-
-    if (!address && !library && contract) {
-      setContract(undefined);
-      setLoadingContract(false);
-    }
-  }, [address, library, contract, loadingContract, contractError]);
+  const { contract, loadingContract, contractError } = useFetchContract(PAPER_CATS_CONTRACT);
 
   return (
     <PaperCatsContractContext.Provider value={{
