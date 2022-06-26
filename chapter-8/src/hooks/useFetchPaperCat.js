66c4721a3ca230538b124a97ebd264e67372575b
Alex <alex@coolcatsnft.com>
2022-06-26 21:39:59 +0100

    feat: update chapter-8 to use fetch-dedupe

--- chapter-8/src/hooks/useFetchPaperCat.js
+++ chapter-8/src/hooks/useFetchPaperCat.js
@@ -1,60 +1,37 @@
 import { useEffect, useState } from "react";
 import { PAPER_CATS_CONTRACT } from "..";
+import { fetchDedupe } from 'fetch-dedupe';
 import useLocalStorage from "./useLocalStorage";
 import usePaperCatsContract from "./usePaperCatsContract";
-
-const requests = new Map();
-const PromiseCache = (key, prom, params) => {
-  let p;
-  if (requests.get(key)) {
-    p = requests.get(key);
-  } else {
-    p = prom.apply(this, params);
-    requests.set(key, p);
-  }
-  
-  return p.then((result) => {
-    return result;
-  }).finally(() => {
-    requests.delete(key);
-  });
-};
 
 export const useFetchPaperCat = (id) => {
   const { contract } = usePaperCatsContract();
   const [loading, setLoading] = useState(false);
-  const [tokenUri, setTokenUri] = useState(null);
+  const [tokenUri, setTokenUri] = useLocalStorage(`papercat-${PAPER_CATS_CONTRACT}-${id}-tokenUri`, '');
   const [paperCat, setPaperCat] = useLocalStorage(`papercat-${PAPER_CATS_CONTRACT}-${id}`, null, 3600);
   const [error, setError] = useState(null);
   
   useEffect(() => {
     if (contract && !paperCat && !tokenUri && !loading && !error) {
       setLoading(true);
-      PromiseCache(
-        `tokenuri-${id}`,
-        contract.methods.tokenURI(id).call
-      ).then((tokenURI) => {
+      contract.methods.tokenURI(id).call().then((tokenURI) => {
         setTokenUri(tokenURI);
       }).catch((err) => {
         setError(err);
       });
     }
-  }, [id, contract, paperCat, tokenUri, loading, error])
+  }, [id, contract, paperCat, tokenUri, setTokenUri, loading, error]);
   
   useEffect(() => {
     if (loading && !paperCat && tokenUri && !error) {
-      PromiseCache(
-        `papercat-${id}`,
-        fetch,
-        [tokenUri, { mode: "cors" }]
+      fetchDedupe(
+        tokenUri,
+        { mode: "cors" }
       ).then((response) => {
         if (response.status !== 200) {
           throw new Error('Invalid response from metadata server');
         }
-        
-        response.clone().json().then((json) => {
-          setPaperCat({...{id: String(id)}, ...json});
-        })
+        setPaperCat({...{id: String(id)}, ...response.data});
       }).catch((err) => {
         setError(err);
       }).finally(() => {
