3eab29eafe0a5503b5008b326e90e08bfcc4e333
Alex <alex@coolcatsnft.com>
2022-06-28 20:54:38 +0100

    feat: update chapter-8 local example

--- chapter-8/src/components/PaperCat.js
+++ chapter-8/src/components/PaperCat.js
@@ -1,22 +1,26 @@
 import { useFetchPaperCat } from "../hooks/useFetchPaperCat";
 
-export function PaperCat({ children, id }) {
-  const { loading, paperCat } = useFetchPaperCat(id);
+export function PaperCat({ id }) {
+  const { error, tokenUri, paperCat } = useFetchPaperCat(id);
+  const loading = !error && !tokenUri;
 
   return (
     <>
-      { loading && <>Loading Paper Cat...</> }
-      { paperCat && (
+      {loading && <>Loading Paper Cat...</>}
+      {paperCat && (
         <>
           <p>{paperCat.name}</p>
           <ul>
-            {paperCat.attributes.map((attr, i) => <li key={i}>{attr.trait_type}: {attr.value}</li>)}
+            {paperCat.attributes.map((attr, i) => (
+              <li key={i}>
+                {attr.trait_type}: {attr.value}
+              </li>
+            ))}
           </ul>
         </>
-      ) }
-      {children}
+      )}
     </>
-  )
+  );
 }
 
 export default PaperCat;
--- chapter-8/src/hooks/useFetchPaperCat.js
+++ chapter-8/src/hooks/useFetchPaperCat.js
@@ -1,44 +1,48 @@
 import { useEffect, useState } from "react";
 import { PAPER_CATS_CONTRACT } from "..";
-import { fetchDedupe } from 'fetch-dedupe';
 import useLocalStorage from "./useLocalStorage";
 import usePaperCatsContract from "./usePaperCatsContract";
 
 export const useFetchPaperCat = (id) => {
   const { contract } = usePaperCatsContract();
-  const [loading, setLoading] = useState(false);
-  const [tokenUri, setTokenUri] = useLocalStorage(`papercat-${PAPER_CATS_CONTRACT}-${id}-tokenUri`, '');
-  const [paperCat, setPaperCat] = useLocalStorage(`papercat-${PAPER_CATS_CONTRACT}-${id}`, null, 3600);
-  const [error, setError] = useState(null);
-  
+  const [tokenUri, setTokenUri] = useLocalStorage(
+    `papercat-${PAPER_CATS_CONTRACT}-${id}-tokenUri`
+  );
+  const [paperCat, setPaperCat] = useLocalStorage(
+    `papercat-${PAPER_CATS_CONTRACT}-${id}-metadata`
+  );
+  const [error, setError] = useState();
+
   useEffect(() => {
-    if (contract && !paperCat && !tokenUri && !loading && !error) {
-      setLoading(true);
-      contract.methods.tokenURI(id).call().then((tokenURI) => {
+    if (contract && !tokenUri && !error) {
+      contract.methods.tokenURI(
+        id
+      ).call().then((tokenURI) => {
         setTokenUri(tokenURI);
       }).catch((err) => {
         setError(err);
       });
     }
-  }, [id, contract, paperCat, tokenUri, setTokenUri, loading, error]);
-  
+  }, [id, contract, tokenUri, setTokenUri, error]);
+
   useEffect(() => {
-    if (loading && !paperCat && tokenUri && !error) {
-      fetchDedupe(
+    if (contract && tokenUri && !paperCat && !error) {
+      fetch(
         tokenUri,
         { mode: "cors" }
       ).then((response) => {
         if (response.status !== 200) {
-          throw new Error('Invalid response from metadata server');
+          throw new Error("Invalid response from metadata server");
         }
-        setPaperCat({...{id: String(id)}, ...response.data});
+
+        response.json().then((data) => {
+          setPaperCat({ ...{ id: String(id) }, ...data });
+        });
       }).catch((err) => {
         setError(err);
-      }).finally(() => {
-        setLoading(false);
       });
     }
-  }, [id, paperCat, loading, tokenUri, error, setPaperCat]);
+  }, [id, contract, tokenUri, paperCat, setPaperCat, error]);
 
-  return { loading, paperCat, error };
-}
+  return { tokenUri, paperCat, error };
+};
